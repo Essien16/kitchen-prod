a85621e192073f779b129ce2620ce996b2935ad1
essien16 <essienjustice@gmail.com>
2024-06-26 16:42:27 +0100

    cleaned up code

--- src/configs/database.js
+++ src/configs/database.js
@@ -1,42 +1,3 @@
-// const mysql = require('mysql2');
-// require("dotenv").config();
-
-// if (process.env.NODE_ENV === "production") {
-//     const dbUrl = process.env.JAWSDB_URL;
-//     connection = mysql.createConnection(dbUrl);
-// }
-//     const pool = mysql.createPool({
-//         host: 'localhost',
-//         user: 'root',
-//         password: 'Theunicorn25$',
-//         database: 'kitchen',
-//         waitForConnections: true,
-//         connectionLimit: 10,
-//         queueLimit: 0
-//     });
-
-
-
-// pool.getConnection((err, connection) => {
-//     if (err) {
-//         if (err.code === 'PROTOCOL_CONNECTION_LOST') {
-//             console.error('Database connection was closed.');
-//         }
-//         if (err.code === 'ER_CON_COUNT_ERROR') {
-//             console.error('Database has too many connections.');
-//         }
-//         if (err.code === 'ECONNREFUSED') {
-//             console.error('Database connection was refused.');
-//         }
-//         console.error('Error connecting to MySQL:', err.stack);
-//         return;
-//     }
-//     console.log('Connected to MySQL as id', connection.threadId);
-//     connection.release();
-// });
-
-// module.exports = pool.promise(); 
-
 const mysql = require('mysql2');
 require("dotenv").config();
 
@@ -49,7 +10,7 @@ if (process.env.NODE_ENV === "production") {
     pool = mysql.createPool({
         host: 'localhost',
         user: 'root',
-        password: 'YourPasswordHere',
+        password: process.env.DB_DEV_PASS,
         database: 'kitchen',
         waitForConnections: true,
         connectionLimit: 10,
